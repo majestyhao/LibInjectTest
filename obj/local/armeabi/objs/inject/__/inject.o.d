299afe830be28e58113cd877962f279652a2c466
Hao Fu <jamesfuhao@gmail.com>
2015-09-13 10:01:19 -0700

    play with ptrace
    play with ptrace

---
+++ obj/local/armeabi/objs/inject/__/inject.o.d
@@ -0,0 +1,218 @@
+/home/hao/workspace/LibInjectTest/obj/local/armeabi/objs/inject/__/inject.o: \
+ /home/hao/workspace/LibInjectTest/jni/../inject.c \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/user.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/ptrace.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ptrace.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/ptrace.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/wait.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/resource.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/resource.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/resource.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/resource.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/wait.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/unistd.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syscall.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/mman.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/mman.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/mman.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/mman-common.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/dlfcn.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/dirent.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/elf.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/exec_elf.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/exec.h \
+ /home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/user.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/ptrace.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ptrace.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/ptrace.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/wait.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/resource.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/resource.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/resource.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/resource.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/wait.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/unistd.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syscall.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/mman.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/mman.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/mman.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/mman-common.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/dlfcn.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/dirent.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/elf.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/exec_elf.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/exec.h:
+
+/home/hao/Android/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
